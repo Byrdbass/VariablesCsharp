ad60bb039b69ef98fb26c5fc9793dbba9418e9fa
ByrdBassPC <byrdbass@gmail.com>
2023-07-19 16:40:26 -0500

    added several variables and learned about adding the m to the decimal, also realized i could not get rid of errors while program is running

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bananas = "That sheet is bananas.";
+            int x = 12;
+            char c = 'C';
+            bool b = true;
+            double y = 100.06;
+            decimal d = .69m;
+            Console.WriteLine($"I've said it {x} times, but {bananas}  These are other variables: " +
+                $"\n {c} \n {b} \n {y} \n {d}");
         }
     }
 }
